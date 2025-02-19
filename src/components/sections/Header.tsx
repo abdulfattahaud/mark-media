c8da66f8b4237586be3b82d67158658513dc0d9e
Abdulfettah <fettahaud@gmail.com>
2025-02-19 22:31:54 +0300

    Header nav UI

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,50 +1,240 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div
-      className='flex h-[4rem] w-full items-center justify-between gap-2 border-b border-[#BABABA33] bg-white px-8 py-4 sm:h-[6.25rem] md:px-16'
-      style={{
-        boxShadow: "0px 4px 100px 0px #C3C3C340",
-      }}
-    >
-      <Image
-        src='/icons/logo.png'
-        width={115}
-        height={60}
-        className='w-[5.5rem] sm:w-[7.25rem]'
-        alt='Media Mark Logo'
-      />
-      <ul className='hidden gap-10 text-base md:flex'>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Expertise</li>
-        <li>How it Works</li>
-        <li>Pricing</li>
-        <li>Testimonial</li>
-        <li>Contact Us</li>
-      </ul>
-      <button className='hidden rounded-[.75rem] bg-primary px-3 py-2 text-sm font-bold text-black sm:block md:rounded-[1.25rem] md:px-6 md:py-4 md:text-base'>
-        Schedule Meeting
-      </button>
-      <button className='rounded-[10px] bg-primary px-4 py-2.5 sm:hidden'>
-        <svg
-          className='h-3.5 w-[1.125rem]'
-          width='18'
-          height='14'
-          viewBox='0 0 18 14'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            d='M1 7H17M1 1H17M1 13H17'
-            stroke='white'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          />
-        </svg>
-      </button>
+    <div>
+      <div className='menu-open-wrapper pointer-events-none fixed right-[48px] top-[48px] z-[100]'>
+        <button className='menu-open flex size-[clamp(3.125rem,0.625rem+5.2083vw,6.875rem)] items-center justify-center rounded-full bg-primary'>
+          <svg
+            className='size-[4.5rem]'
+            width='72'
+            height='72'
+            viewBox='0 0 72 72'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M35.5537 63.36L8.10767 35.6935L35.5537 8.02686' stroke='#273F2B' strokeWidth='8.64319' />
+            <path d='M8.00531 35.5303L59.4932 35.5303' stroke='#273F2B' strokeWidth='8.64319' />
+          </svg>
+        </button>
+      </div>
+      <div className='menu-screen-wrapper pointer-events-none fixed left-0 top-0 z-[1001] flex w-full justify-end bg-[#3837373D] opacity-0'>
+        <div className='menu-screen w-ful flex h-[100dvh] rounded-l-[3.125rem] bg-primary px-[3.125rem] py-[4.5rem] md:w-[45%] md:min-w-[40rem]'>
+          <button
+            // onClick={() => menuOut()}
+            className='close-but absolute right-4 top-4 z-[10] flex size-[3.5rem] items-center justify-center rounded-full bg-white'
+          >
+            <svg
+              className='size-[1.25rem]'
+              width='19'
+              height='19'
+              viewBox='0 0 19 19'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M17.1233 1.87669C17.6725 2.42587 17.6725 3.31626 17.1233 3.86543L3.86502 17.1237C3.31585 17.6729 2.42546 17.6729 1.87628 17.1237C1.32711 16.5745 1.32711 15.6841 1.87628 15.1349L15.1345 1.87669C15.6837 1.32752 16.5741 1.32752 17.1233 1.87669Z'
+                fill='black'
+              />
+              <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M17.1228 17.1233C16.5736 17.6724 15.6833 17.6724 15.1341 17.1233L1.87583 3.86502C1.32665 3.31585 1.32665 2.42546 1.87583 1.87628C2.425 1.32711 3.31539 1.32711 3.86457 1.87628L17.1228 15.1345C17.672 15.6837 17.672 16.5741 17.1228 17.1233Z'
+                fill='black'
+              />
+            </svg>
+          </button>
+          <div className='flex w-full flex-1 flex-col justify-between gap-[48px] overflow-y-scroll sm:w-auto'>
+            <div className='flex h-full flex-col justify-between gap-[1.625rem]'>
+              <ul className='items-list flex cursor-pointer flex-col gap-4 text-[2.75rem] font-bold leading-[110%]'>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>Home</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>About Us</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>Expertise</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>How it Works</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>Pricing</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>Testimonial</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+                <li className='flex cursor-pointer items-end gap-8 leading-[100%]'>
+                  <span className='block leading-[100%]'>Contact Us</span>
+                  <svg
+                    className='size-4'
+                    width='16'
+                    height='16'
+                    viewBox='0 0 16 16'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.1687 2.8301L0 13.9988L2.0012 16L13.17 4.8311V13.8675H16.0001V0H2.1327V2.8301H11.1687Z'
+                      fill='black'
+                    />
+                  </svg>
+                </li>
+              </ul>
+              <div className='flex flex-col gap-5'>
+                <Image
+                  src='/icons/logo.png'
+                  alt='Mark Media'
+                  width={115}
+                  height={60}
+                  className='w-[5.5rem] sm:w-[7.25rem]'
+                />
+                <span className='text-[1.25rem] font-medium'>Where Strategy Meets Success</span>
+                <div className='flex gap-[.625rem]'>
+                  <Link className='cursor-pointer' href='https://www.instagram.com/markmedia.id/' target='_blank'>
+                    <Image src='/icons/instagram.svg' alt='Instagram' width={24} height={24} />
+                  </Link>
+                  <Link className='cursor-pointer' href='https://www.facebook.com/markmedia.id/' target='_blank'>
+                    <Image src='/icons/facebook.svg' alt='Facebook' width={24} height={24} />
+                  </Link>
+                  <Link className='cursor-pointer' href='https://www.twitter.com/markmedia.id/' target='_blank'>
+                    <Image src='/icons/twitter.svg' alt='Twitter' width={24} height={24} />
+                  </Link>
+                  <Link className='cursor-pointer' href='https://www.tiktok.com/@markmedia.id' target='_blank'>
+                    <Image src='/icons/tiktok.svg' alt='TikTok' width={24} height={24} />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className='flex h-[4rem] w-full items-center justify-between gap-2 border-b border-[#BABABA33] bg-white px-8 py-4 sm:h-[6.25rem] md:px-16'
+        style={{
+          boxShadow: "0px 4px 100px 0px #C3C3C340",
+        }}
+      >
+        <Image
+          src='/icons/logo.png'
+          width={115}
+          height={60}
+          className='w-[5.5rem] sm:w-[7.25rem]'
+          alt='Media Mark Logo'
+        />
+        <ul className='hidden gap-10 text-base md:flex'>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Expertise</li>
+          <li>How it Works</li>
+          <li>Pricing</li>
+          <li>Testimonial</li>
+          <li>Contact Us</li>
+        </ul>
+        <button className='hidden rounded-[.75rem] bg-primary px-3 py-2 text-sm font-bold text-black sm:block md:rounded-[1.25rem] md:px-6 md:py-4 md:text-base'>
+          Schedule Meeting
+        </button>
+        <button className='rounded-[10px] bg-primary px-4 py-2.5 sm:hidden'>
+          <svg
+            className='h-3.5 w-[1.125rem]'
+            width='18'
+            height='14'
+            viewBox='0 0 18 14'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M1 7H17M1 1H17M1 13H17'
+              stroke='white'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
